2c36d2f4da1b6d3fa786e45c77a37152d9badd3c
sundays <327764115@qq.com>
2018-04-10 19:35:07 +0800

    1

--- scala/src/main/scala/Main.scala
+++ scala/src/main/scala/Main.scala
@@ -1,5 +1,11 @@
+import com.alibaba.fastjson.JSON
+import org.apache.spark.{SparkConf, SparkContext}
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println(DateUtils.getDate)
+    val conf = new SparkConf().setAppName("WordCount").setMaster("local")
+    val sc = new SparkContext(conf)
+    val line = sc.textFile("/home/sundays/Desktop/新建文本 4.txt")
+    line.map(_.split(",")(0)).foreach(println)
   }
 }
